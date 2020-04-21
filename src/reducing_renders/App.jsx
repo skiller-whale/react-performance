5209112d63c9090dfecf5338d131c3c2a7ce1b89
H C <1674771+hcarver@users.noreply.github.com>
2020-04-21 13:31:56 +0100

    Reducing renders code

---
+++ src/reducing_renders/App.jsx
@@ -0,0 +1,172 @@
+import React from 'react';
+import faker from 'faker';
+
+class CustomerRow extends React.Component {
+  render() {
+    return <tr>
+      <td>
+        {this.props.customer.name.first}
+        {' '}
+        {this.props.customer.name.last}
+      </td>
+      <td>
+        {this.props.customer.email}
+      </td>
+      <td>
+        $
+        {this.props.customer.amountSpent}
+      </td>
+    </tr>
+  }
+}
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+
+    const customers = [];
+    for (let i = 0; i < 200; i += 1) {
+      customers.push(this.generateRecord());
+    }
+    this.sortCustomers(customers);
+
+    this.state = {
+      customers: customers,
+      newFirstName: '',
+      newLastName: '',
+      newEmail: '',
+      newAmountSpent: '',
+    };
+    this.addCustomerClicked = this.addCustomerClicked.bind(this);
+    this.firstNameUpdated = this.firstNameUpdated.bind(this);
+    this.lastNameUpdated = this.lastNameUpdated.bind(this);
+    this.emailUpdated = this.emailUpdated.bind(this);
+    this.amountSpentUpdated = this.amountSpentUpdated.bind(this);
+  }
+
+  addCustomerClicked(event) {
+    event.preventDefault();
+
+    const {
+      newFirstName, newLastName, newEmail, newAmountSpent,
+    } = this.state;
+
+    const newCustomer = {
+      name: {
+        first: newFirstName,
+        last: newLastName,
+      },
+      email: newEmail,
+      amountSpent: newAmountSpent,
+    };
+
+    this.addCustomer(newCustomer);
+
+    this.setState(
+      {
+        newFirstName: '',
+        newLastName: '',
+        newEmail: '',
+        newAmountSpent: '',
+      }
+    );
+  }
+
+  firstNameUpdated(e) {
+    this.setState({ newFirstName: e.target.value });
+  }
+
+  lastNameUpdated(e) {
+    this.setState({ newLastName: e.target.value });
+  }
+
+  emailUpdated(e) {
+    this.setState({ newEmail: e.target.value });
+  }
+
+  amountSpentUpdated(e) {
+    this.setState({ newAmountSpent: e.target.value });
+  }
+
+  generateRecord() {
+    return {
+      name: {
+        first: faker.name.firstName(),
+        last: faker.name.lastName(),
+      },
+      email: faker.internet.email(),
+      amountSpent: faker.finance.amount(),
+    };
+  }
+
+  sortCustomers(customers) {
+    customers.sort((p1, p2) => {
+      if (p1.name.last === p2.name.last) {
+        return p1.name.first.localeCompare(p2.name.first);
+      }
+      return p1.name.last.localeCompare(p2.name.last);
+    });
+  }
+
+  addCustomer(newCustomer) {
+    const newCustomers = [newCustomer, ...this.state.customers];
+    this.sortCustomers(newCustomers);
+
+    this.setState(
+      {
+        customers: newCustomers
+      }
+    )
+  }
+
+  render() {
+    const { customers, } = this.state;
+    const rows = customers.map(
+      customer => {
+        return <CustomerRow customer={customer} />;
+      }
+    );
+
+    const {
+      newFirstName, newLastName, newEmail, newAmountSpent,
+    } = this.state;
+
+    return (
+      <div className="container">
+        <h5>Edit me in src/reducing_renders/App.jsx</h5>
+        <h1>Our customers</h1>
+        <div className="row">
+          <div className="col-lg-6 col-md-12">
+            <form className="form" onSubmit={this.addCustomerClicked}>
+              <div className="form-group">
+                <input placeholder="first name" className="form-control" onChange={this.firstNameUpdated} value={newFirstName} />
+                <input placeholder="last name" className="form-control" onChange={this.lastNameUpdated} value={newLastName} />
+                <input placeholder="email" className="form-control" onChange={this.emailUpdated} value={newEmail} />
+                <input placeholder="amount spent" className="form-control" onChange={this.amountSpentUpdated} value={newAmountSpent} />
+              </div>
+              <div className="form-group">
+                <button type="submit" className="btn btn-default">
+                  Create customer {newFirstName} {newLastName}
+                </button>
+              </div>
+            </form>
+          </div>
+        </div>
+        <table className="table table-striped table-bordered table-hover">
+          <thead>
+            <tr>
+              <th>Name</th>
+              <th>Email</th>
+              <th>Amount spent</th>
+            </tr>
+          </thead>
+          <tbody>
+            { rows }
+          </tbody>
+        </table>
+      </div>
+    );
+  }
+}
+
+export default App;
